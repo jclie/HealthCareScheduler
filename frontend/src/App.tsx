aac9f659fe43de4e7f99d7ebb8d0a256232c5342
jclie <jtong0003@mymail.lausd.net>
2026-08-18 19:10:34 -0700

    project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <head> HealthCare Scheduler </head>
+      <p> Scheduling System </p>
+    </div>
+  );
+}
+
+export default App
